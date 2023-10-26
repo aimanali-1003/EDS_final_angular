41de832a88f34870bcdbe9b1292d25489dcde977
Sameer Shahzad <sameershahzad872@gmail.com>
2023-10-26 11:45:29 -0400

    Added template detail container on the basis of template selected on Job create screen

--- src/app/components/create-job/create-job.component.ts
+++ src/app/components/create-job/create-job.component.ts
@@ -3,11 +3,11 @@ import { JobService } from 'src/app/services/job.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { JobDataModel } from 'src/app/model/JobModel';
-import { DataService } from 'src/app/services/data.service';
 import { DataTemplateModel } from 'src/app/model/DataTemplateModel';
 import { ClientService } from 'src/app/services/client.service';
 import { clientDataModel } from 'src/app/model/ClientModel';
 import { OrgService } from 'src/app/services/org.service';
+import { DataService } from 'src/app/services/data.service';
 import { OrgDataModel } from 'src/app/model/OrgDataModel';
 import { DataRecipientService } from 'src/app/services/data-recipient.service';
 import { RecipientTypeDataModel } from 'src/app/model/DataRecipientType.model';
@@ -23,19 +23,20 @@ import { FileFormatService } from 'src/app/services/file-format.service';
 })
 export class CreateJobComponent implements OnInit {
   extractionDate!: Date;
-  
-  jobs: any[] = [];  
+
+  jobs: any[] = [];
   showTimeInput: boolean = false;
   selectedDays: { [key: string]: boolean } = {}; // Initialize selectedDays object
-  isViewOnly:boolean = false;
+  isViewOnly: boolean = false;
   jobId: string = '';
-  isEdit:boolean = false;
+  isEdit: boolean = false;
   jobData: JobDataModel = new JobDataModel();
   frequencyData: FrequencyDataModel = new FrequencyDataModel();
   templateData: DataTemplateModel = new DataTemplateModel();
   templates: any[] = [];
-  selectedTemplates: any; 
-  selectedOrganization: any; 
+  template: DataTemplateModel = new DataTemplateModel();
+  selectedTemplates: any;
+  selectedOrganization: any;
   orgs: any[] = [];
   organizationClients: clientDataModel[] = [];
   selectedOrganizationId: number | null = null;
@@ -58,13 +59,13 @@ export class CreateJobComponent implements OnInit {
   startTime!: string;
   endTime!: string;
   selectedItems: string[] = [];
-  
+  templateId: string = '';
+
+  columns: string[] = [];
 
 
-  
-  
   constructor(
-    private jobService: JobService, 
+    private jobService: JobService,
     private router: Router,
     private snackBar: MatSnackBar,
     private route: ActivatedRoute,
@@ -95,11 +96,11 @@ export class CreateJobComponent implements OnInit {
     this.loadTemplatesDropDown();
     this.loadOrganizations();
 
-    this.route.params.subscribe((params) =>{
+    this.route.params.subscribe((params) => {
       this.jobId = params['jobId'];
       this.isViewOnly = params['isViewOnly'];
-      
-      if(this.jobId != undefined && this.jobId != "" && this.jobId != null && this.jobId != ''){
+
+      if (this.jobId != undefined && this.jobId != "" && this.jobId != null && this.jobId != '') {
         this.isEdit = true;
         this.loadJobData();
       }
@@ -110,23 +111,39 @@ export class CreateJobComponent implements OnInit {
   loadJobData(): void {
     this.jobService.getJob(this.jobId).subscribe((jobData: any) => {
       this.jobData = jobData;
-      
+
     });
   }
-  
+
   onOrganizationSelected(organizationId: number) {
     this.clients = [];
-    this.organizations.organizationID = organizationId; 
+    this.organizations.organizationID = organizationId;
     this.loadOrganizationClients(organizationId);
   }
-  
-  
+
 
   loadTemplatesDropDown(): void {
     this.dataService.getDataTemplates().subscribe((templates: any[]) => {
-      this.templates = templates; 
+      this.templates = templates;
     });
   }
+
+  loadTemplates(templateId: string) {
+    this.templateId = templateId;
+    this.dataService.getTemplate(templateId).subscribe((template: any) => {
+      this.template = template;
+      console.log(this.template.templateName);
+      console.log(this.template.categoryID);
+      this.viewTemplateColumns();
+    });
+  }
+  viewTemplateColumns() {
+    this.dataService.getColumnsOfTemplate(this.templateId).subscribe((column: string[]) => {
+      this.columns = column;
+      console.log("Columns", this.columns);
+    });
+  }
+
 
   loadOrganizations(): void {
     this.clientService.getOrgs().subscribe((orgs: any[]) => {
@@ -144,11 +161,11 @@ export class CreateJobComponent implements OnInit {
     }
   }
 
-  goToJobScreen() { 
+  goToJobScreen() {
     this.router.navigate(['/jobs']);
   }
 
-  ValidateFormFields(){
+  ValidateFormFields() {
     if (!this.jobData.jobType) {
       this.snackBar.open('Job Name is required.', 'Close', {
         duration: 3000,
@@ -165,44 +182,44 @@ export class CreateJobComponent implements OnInit {
     this.jobData.recipientTypeId = this.selectedDataRecipientTypeId;
     this.jobData.frequencyType = this.extractionFrequency;
     this.jobData.fileFormatType = this.fileFormatstore;
- 
-     if(this.isEdit){
- 
-       this.jobService.updateJob(this.jobId, this.jobData).subscribe(
-         (response: any) => {
-           this.snackBar.open('Job updated successfully', 'Close', {
-             duration: 2000,
-           });
-     
-           this.router.navigate(['/jobs']);
-         },
-         (error: any) => {
-           this.snackBar.open('Error updating job:' + error.error, 'Close', {
-             duration: 2000,
-           });
-         }
-       );
-     }else{
+
+    if (this.isEdit) {
+
+      this.jobService.updateJob(this.jobId, this.jobData).subscribe(
+        (response: any) => {
+          this.snackBar.open('Job updated successfully', 'Close', {
+            duration: 2000,
+          });
+
+          this.router.navigate(['/jobs']);
+        },
+        (error: any) => {
+          this.snackBar.open('Error updating job:' + error.error, 'Close', {
+            duration: 2000,
+          });
+        }
+      );
+    } else {
 
       console.log(this.jobData);
-       this.jobService.createJob(this.jobData).subscribe((response: any) => {
-         
-         this.snackBar.open('Job created successfully', 'Close', {
-           duration: 3000,
-         });
- 
-         this.router.navigate(['/jobs']);
-       },
-       (error) => {
-         console.error('Error creating job:', error);
-         // Handle error and show an error message
-         this.snackBar.open('Error creating job: ' + error.error, 'Close', {
-           duration: 3000, // Duration in milliseconds
-         });
-       }
-       );
-     }
-   
-   }
+      this.jobService.createJob(this.jobData).subscribe((response: any) => {
+
+        this.snackBar.open('Job created successfully', 'Close', {
+          duration: 3000,
+        });
+
+        this.router.navigate(['/jobs']);
+      },
+        (error) => {
+          console.error('Error creating job:', error);
+          // Handle error and show an error message
+          this.snackBar.open('Error creating job: ' + error.error, 'Close', {
+            duration: 3000, // Duration in milliseconds
+          });
+        }
+      );
+    }
 
   }
+
+}
